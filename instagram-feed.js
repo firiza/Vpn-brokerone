a36d714a5d935bea3fbbcf50b62556fb3ba524ba
Freddy Iriza <firiza91@gmail.com>
2018-11-12 18:23:11 -0500

    pequeñas mejoras

--- instagram-feed.js
+++ instagram-feed.js
@@ -4,11 +4,11 @@ $(document).ready(function() {
     var userFeed = new Instafeed({
         get: 'user',
         userId: '1254026799',
-        limit: 12,
+        limit: 4,
         resolution: 'standard_resolution',
         accessToken: '1254026799.1677ed0.600c90cab90645ebb4b55115ebe763d1',
         sortBy: 'most-recent',
-        template: '<div class="col-lg-3 instaimg"><a href="{{image}}" title="{{caption}}" target="_blank"><img src="{{image}}" alt="{{caption}}" class="img-fluid"/></a></div>',
+        template: '<div class=" img-thumbnail insta-style-thumb"><a href="{{link}}" target="_blank"><img src="{{image}}" class="img-fluid"/></a></div>',
     });
 
 
@@ -16,13 +16,13 @@ $(document).ready(function() {
 
     
     // This will create a single gallery from all elements that have class "gallery-item"
-    $('.gallery').magnificPopup({
-        type: 'image',
-        delegate: 'a',
-        gallery: {
-            enabled: true
-        }
-    });
+    // $('.gallery').magnificPopup({
+    //     type: 'image',
+    //     delegate: 'a',
+    //     gallery: {
+    //         enabled: true
+    //     }
+    // });
 
 
 });
